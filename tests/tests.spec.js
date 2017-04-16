82af1ca215fdb8f43dc0af68d3bd051e00dbf68e
yannunes <yannunes@gmail.com>
2017-04-16 12:39:34 -0300

    No comment on this

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -4,7 +4,7 @@ const chai = require('chai')
 var expect = chai.expect
 
 describe('First build and teste', ()=>{
-  if('should built without error', function(done) {
+  it('should built without error', function(done) {
     console.log('built without error')
     done()
   })
