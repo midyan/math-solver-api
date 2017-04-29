18be56ea8a7945a7179ec0d543e125fbbf5f539a
yannunes <yannunes@gmail.com>
2017-04-29 15:39:07 -0300

    Better test coverage for new parameter error handling

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -7,7 +7,7 @@ const mainFile = require('../index.js')
 var expect = chai.expect
 
 describe('First build and test', () => {
-  it('should built without error', function(done) {
+  it('should built without error', function (done) {
     console.log('built without error')
     done()
   })
@@ -15,7 +15,74 @@ describe('First build and test', () => {
 
 describe('Tesseract testing', () => {
   describe('should recognize small text from web', () => {
-    it('return image content from base64', function(done){
+    it('return image content from base64', function (done) {
+      this.timeout(20000)
+      fs.readFile(
+        appDir + '/texts/clean_photo_test.jpg',
+        (err, result) => {
+          if (err) {
+            done(err)
+          } else {
+            console.log('Executing tesseract \n')
+            mainFile
+              .execTesseract({
+                data: new Buffer(result).toString('base64'),
+                lang: 'eng'
+              })
+              .then(result => {
+                expect(result).to.be.an('object')
+                console.log(result)
+                done()
+              })
+              .catch(err => {
+                if (err) done(err)
+              })
+          }
+        }
+      )
+    })
+    it('should return options parameter error', function (done) {
+      this.timeout(5000)
+      mainFile
+        .execTesseract()
+        .then(result => {
+          expect(result).to.not.be.ok
+          done(result)
+        })
+        .catch(err => {
+          if (err) {
+            var mErr = null
+            try {
+              expect(err.error).to.equal('Options parameter is required')
+            } catch (error) {
+              mErr = error
+            }
+            done(mErr)
+          }
+        })
+    })
+    it('should return data parameter error', function(done) {
+      mainFile
+        .execTesseract({
+          lang: 'eng'
+        })
+        .then(result =>{
+          expect(result).to.not.be.ok
+          done(result)
+        })
+        .catch(err => {
+          if (err) {
+            var mErr = null
+            try {
+              expect(err.error).to.equal('Data parameter is required')
+            } catch (error) {
+              mErr = error
+            }
+            done(mErr)
+          }
+        })
+      })
+    it('should return lang parameter error', function(done) {
       this.timeout(20000)
       fs.readFile(
         appDir + '/texts/clean_photo_test.jpg',
@@ -25,19 +92,24 @@ describe('Tesseract testing', () => {
             done(err)
           } else {
             console.log('Executing tesseract \n')
-            console.log(result)
             mainFile
               .execTesseract({
-                data: new Buffer(result).toString('base64'),
-                lang: 'eng'
+                data: new Buffer(result).toString('base64')
               })
               .then(result =>{
-                expect(result).to.be.an('object')
-                console.log(result)
-                done()
+                expect(result).to.not.be.ok
+                done(result)
               })
               .catch(err => {
-                if(err) done(err)
+                if (err) {
+                  var mErr = null
+                  try {
+                    expect(err.error).to.equal('Language parameter is required')
+                  } catch (error) {
+                    mErr = error
+                  }
+                  done(mErr)
+                }
               })
           }
         }
